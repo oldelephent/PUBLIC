a9b1c50ca856eb4fb5adcf3791a976d5ab1d97ae
oldelephent <146737084+oldelephent@users.noreply.github.com>
2023-12-07 21:45:22 +0530

    update1

---
+++ POWERSHELL/RDP_PORT_CHANGE.ps1
@@ -0,0 +1,3 @@
+﻿#Get-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name Portnumber
+
+Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name PortNumebr 
