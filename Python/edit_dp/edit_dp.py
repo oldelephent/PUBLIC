1a399495923c7e76df6fb9c7bab3403a7286ec69
oldelephent <146737084+oldelephent@users.noreply.github.com>
2025-03-05 22:51:00 +0530

    .

---
+++ Python/edit_dp/edit_dp.py
@@ -0,0 +1,20 @@
+
+file_to_read = r""
+file_to_write = r""
+
+
+with open(file_to_read,'r')as f1:
+    file = csv.DictReader(f1)
+    header = file.fieldnames
+    data = []
+
+    for row in file:
+        #if row['Series'] not in ["A","EQ"] and re.match("^WBF",row['Client']):
+        if row['Series'] not in ["A","EQ"] or re.match("^WBF",row['Client']):
+            row['Open Authorize Quantity'] = 0
+        data.append(row)
+            
+with open (file_to_write,'w',newline='')as f2:
+    write = csv.DictWriter(f2, fieldnames=header)
+    write.writeheader()
+    write.writerows(data)
