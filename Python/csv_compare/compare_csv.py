e5f9b42e67731a79c96f2e18b4e39882c1a09cb0
oldelephent <146737084+oldelephent@users.noreply.github.com>
2025-03-11 22:57:06 +0530

    .

--- Python/csv_compare/compare_csv.py
+++ Python/csv_compare/compare_csv.py
@@ -4,12 +4,30 @@ file1 = r"1.csv"
 file2 = r"2.csv"
 
 
-with open(file1,'r')as f1, open(file2,'r')as f2:
-    read1 = tuple(csv.reader(f1))
-    read2 = tuple(csv.reader(f2))
-    for i,j in zip(read1,read2):
-        if i == j:
-            print(f"{i},{j}all good")
-        else:
-            print(f"{i},{j}not good")
+# with open(file1,'r')as f1, open(file2,'r')as f2:
+#     read1 = tuple(csv.reader(f1))
+#     read2 = tuple(csv.reader(f2))
+#     for i,j in zip(read1,read2):
+#         if i == j:
+#             print(f"{i},{j}all good")
+#         else:
+#             print(f"{i},{j}not good")
 
+
+
+
+
+####################################        color coded
+
+# good_color = '\033[32m'  # Green
+# not_good_color = '\033[31m'  # Red
+# reset_color = '\033[0m'  # Reset to default color
+
+# with open(file1, 'r') as f1, open(file2, 'r') as f2:
+#     read1 = tuple(csv.reader(f1))
+#     read2 = tuple(csv.reader(f2))
+#     for i, j in zip(read1, read2):
+#         if i == j:
+#             print(f"{good_color}{i},{j} all good{reset_color}")
+#         else:
+#             print(f"{not_good_color}{i},{j} not good{reset_color}")
